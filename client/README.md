a77b4768417c3924e96b9a319ec950a57f79a9bf
ashutoshdronni <droniashutosh2@gmail.com>
2026-01-23 10:46:26 +0530

    nn

--- client/README.md
+++ client/README.md
@@ -1,1 +1,20 @@
-mm
+#step 1
+cd server 
+npm install
+npm run server
+
+#step 2
+cd client 
+npm install
+npm run dev
+
+
+
+clodinary id,api,password required
+jwt key required
+cluster mongodb reuired 
+
+
+when you do /seller than put this 
+SELLER_EMAIL=admin@example.com
+SELLER_PASSWORD=admin@12345
--- server/configs/db.js
+++ server/configs/db.js
@@ -1,13 +1,12 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-    try{
-        mongoose.connection.on('connected', ()=>console.log("Database connected"));
-        await mongoose.connect(`${process.env.MONGODB_URL}/greencart`)
-    } catch(error){
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => console.log("Database connected"));
+        await mongoose.connect(process.env.MONGODB_URL); // Remove the template literal
+    } catch(error) {
         console.error(error.message);
     }
 }
-
 
 export default connectDB;
